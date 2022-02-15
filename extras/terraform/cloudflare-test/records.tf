8a044e340c30c07162e4b64a6eb3a1b22335d50b
symbiontik <symbiontik@gmail.com>
2022-02-15 11:33:47 -0600

    update tf

--- extras/terraform/cloudflare-test/records.tf
+++ extras/terraform/cloudflare-test/records.tf
@@ -1,28 +1,8 @@
-# Add a record to the domain
-resource "cloudflare_record" "ipv4" {
-  name    = "ipv4"
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = var.PUBLIC_IP_ADDRESS
-  proxied = true
-  type    = "A"
-  ttl     = 1
-}
-
-# Add a record to the domain
-resource "cloudflare_record" "root" {
-  name    = var.BOOTSTRAP_CLOUDFLARE_DOMAIN
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${var.PUBLIC_IP_ADDRESS}"
-  proxied = true
-  type    = "CNAME"
-  ttl     = 1
-}
-
-# Add a record to the domain
+# Add a record for traefik to your domain
 resource "cloudflare_record" "traefik2" {
   name    = "traefik2"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${var.PUBLIC_IP_ADDRESS}"
+  value   = var.PUBLIC_IP_ADDRESS
   proxied = true
   type    = "CNAME"
   ttl     = 1
