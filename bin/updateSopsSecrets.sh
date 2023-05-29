a53d52c4d76cafa260245aab77acfe042a1143e8
Francis Araujo <francis.araujo@gmail.com>
2023-05-28 22:13:30 -0300

    Automating secrets generation for rancher

--- bin/updateSopsSecrets.sh
+++ bin/updateSopsSecrets.sh
@@ -14,11 +14,20 @@ envsubst < "${PROJECT_DIR}/tmpl/cluster/cluster-secrets.sops.yaml" \
     > "${PROJECT_DIR}/cluster/base/cluster-secrets.sops.yaml"
 envsubst < "${PROJECT_DIR}/tmpl/cluster/cert-manager-secret.sops.yaml" \
     > "${PROJECT_DIR}/cluster/core/cert-manager/secret.sops.yaml"
+envsubst < "${PROJECT_DIR}/tmpl/cluster/cloudflare-originca-sops.yaml" \
+    > "${PROJECT_DIR}/cluster/core/rancher/cloudflare-originca-sops.yaml"
+envsubst < "${PROJECT_DIR}/tmpl/cluster/rancher-bootstrap-sops.yaml" \
+    > "${PROJECT_DIR}/cluster/core/rancher/rancher-bootstrap-sops.yaml"
+
 # encrypt sensitive files
 sops --encrypt --in-place "${PROJECT_DIR}/cluster/base/cluster-secrets.sops.yaml"
 sops --encrypt --in-place "${PROJECT_DIR}/cluster/core/cert-manager/secret.sops.yaml"
+sops --encrypt --in-place "${PROJECT_DIR}/cluster/core/rancher/cloudflare-originca-sops.yaml"
+sops --encrypt --in-place "${PROJECT_DIR}/cluster/core/rancher/rancher-bootstrap-sops.yaml"
 
 git add cluster/base/cluster-secrets.sops.yaml
 git add cluster/core/cert-manager/secret.sops.yaml
+git add cluster/core/rancher/cloudflare-originca-sops.yaml
+git add cluster/core/rancher/rancher-bootstrap-sops.yaml
 
 git commit -m "Updating Sops secrets"
