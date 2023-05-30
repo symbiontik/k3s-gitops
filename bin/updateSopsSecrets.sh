1ede77f3b2eaf80278b3379fefbbbf58cc270d1b
franaraujo77 <francis.araujo@gmail.com>
2023-05-30 14:49:47 -0300

    Network config (#104)
    * Configure secret to get value from github
    
    * Remove prune configuration
    
    * Change variable configuration
    
    * Add new variables into cluster secrets
    
    * Update steps to describe new configurations
    
    * Configure apps from secrets
    
    * Updating Sops secrets
    
    * Add script to automate flux installation into cluster
    
    * Update secrets list
    
    * Removing CRDs installation with helm release
    
    * Fix cloudflare ca issuer helm configuration
    
    * Remove rbac creation
    
    * Remove rbac creation
    
    * Remove api service creation
    
    * Remove metrics-server as it is already installed
    
    * Configure rancher ssl
    
    * More rancher to cattle-system namespace
    
    * Fix: putting objects into rancher ns
    
    * Fix: putting objects into rancher ns
    
    * Publish secret into right ns
    
    * Create default issuers
    
    * Fix ns name
    
    * Removing lets encript issuers
    
    * Fix secret type
    
    * Using kustomize to create secret for rancher
    
    * Disable name suffix hashes for secret
    
    * Automating secrets generation for rancher
    
    * Automating secrets generation for rancher
    
    * Change rancher secret files to use env type
    
    * Fix identation
    
    * Fix file names
    
    * Fix file references
    
    * Fix env file  references
    
    * Remove sops encrypted files in env format from resources list
    
    * Update cloudflare api token
    
    * Update cloudflare api token
    
    * Transfer bootstrap to secret
    
    * Update secrets information for rancher
    
    * Reducing secret to only one value
    
    * Update rancher configurations for bootstrap
    
    * Update rancher configurations for bootstrap
    
    * Add doamin as a configuration for rancher
    
    * Traefik removal
    
    * Update prometheus network exposition

--- bin/updateSopsSecrets.sh
+++ bin/updateSopsSecrets.sh
@@ -14,20 +14,30 @@ envsubst < "${PROJECT_DIR}/tmpl/cluster/cluster-secrets.sops.yaml" \
     > "${PROJECT_DIR}/cluster/base/cluster-secrets.sops.yaml"
 envsubst < "${PROJECT_DIR}/tmpl/cluster/cert-manager-secret.sops.yaml" \
     > "${PROJECT_DIR}/cluster/core/cert-manager/secret.sops.yaml"
-envsubst < "${PROJECT_DIR}/tmpl/cluster/cloudflare-originca-sops.env" \
-    > "${PROJECT_DIR}/cluster/core/rancher/cloudflare-originca-sops.env"
 envsubst < "${PROJECT_DIR}/tmpl/cluster/rancher-bootstrap-sops.env" \
     > "${PROJECT_DIR}/cluster/core/rancher/rancher-bootstrap-sops.env"
+
+# create unique cloudflare resources
+envsubst < "${PROJECT_DIR}/tmpl/cluster/cloudflare-originca-sops.env" \
+    > "${PROJECT_DIR}/cluster/core/rancher/cloudflare-originca-sops.env"
+envsubst < "${PROJECT_DIR}/tmpl/cluster/cloudflare-originca-sops.env" \
+    > "${PROJECT_DIR}/cluster/observability/prometheus-with-node-exporter/cloudflare-originca-sops.env"
 
 # encrypt sensitive files
 sops --encrypt --in-place "${PROJECT_DIR}/cluster/base/cluster-secrets.sops.yaml"
 sops --encrypt --in-place "${PROJECT_DIR}/cluster/core/cert-manager/secret.sops.yaml"
-sops --encrypt --in-place "${PROJECT_DIR}/cluster/core/rancher/cloudflare-originca-sops.env"
 sops --encrypt --in-place "${PROJECT_DIR}/cluster/core/rancher/rancher-bootstrap-sops.env"
+
+# encrypt cloudflare resource
+sops --encrypt --in-place "${PROJECT_DIR}/cluster/core/rancher/cloudflare-originca-sops.env"
+sops --encrypt --in-place "${PROJECT_DIR}/cluster/observability/prometheus-with-node-exporter/cloudflare-originca-sops.env"
 
 git add cluster/base/cluster-secrets.sops.yaml
 git add cluster/core/cert-manager/secret.sops.yaml
-git add cluster/core/rancher/cloudflare-originca-sops.env
 git add cluster/core/rancher/rancher-bootstrap-sops.env
+
+# add cloudflare resources
+git add cluster/core/rancher/cloudflare-originca-sops.env
+git add cluster/observability/prometheus-with-node-exporter/cloudflare-originca-sops.env
 
 git commit -m "Updating Sops secrets"
